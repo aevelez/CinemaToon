4c08fc477f490451e98df4e0ab6277658a29e550
Alex Edwin Velez Velez <aleno23@outlook.com>
2019-11-05 10:04:40 -0500

    Add Interface an Entity

---
+++ CinemaToon/CinemaToon.Entities/Theaters/Theater.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace CinemaToon.Entities.Theaters
+{
+    public class Theater
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Name")]
+        public string Name { get; set; }
+
+        [DisplayName("Full Address")]
+        public string FullAddress { get; set; }
+
+    }
+}
---
+++ CinemaToon/CinemaToon.Utilities.Abstractions/Interfaces/ITheatersService.cs
@@ -0,0 +1,13 @@
+﻿using CinemaToon.Entities.Theaters;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaToon.Utilities.Abstractions.Interfaces
+{
+    public interface ITheatersService
+    {
+        IEnumerable<Theater> GetTheaters();
+    }
+}
